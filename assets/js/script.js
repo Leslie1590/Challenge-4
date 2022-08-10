f1e1655e8fa041ca29cb3374fd59e758d0c7d174
Crayen <transparent_legacy@yahoo.com>
2022-08-09 21:30:46 -0500

    more working on javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,70 @@
 var questionsEl = document.querySelector("#page-content");
-var answersEl = document.querySelector("answer-button")
-var TimerEl = document.querySelector("timer")
+var answersEl = document.querySelector("#answer-button");
+var TimerEl = document.querySelector("#timer");
+var button1El = document.querySelector("#answer1");
+var button2El = document.querySelector("#answer2");
+var button3El = document.querySelector("answer3");
 
 
-var quizMe = function(event) {
+//questions array
+
+var questionsArray =[
+    {
+        question1: "Some nerdy question here?",
+        answers: ["answer 1", "answer2", "answer3"]
+    },
+
+    {
+        question2: "Question two is not so nerdy?",
+        answers: ["answer1", "answer2", "answer3"]
+    },
+    {
+        question3: "Question three is completely out of left field?",
+        answer: ["answer1", "answer2", "answer3"]
+    }
     
-}
+]
 
+//question 1 function
+function questionNumberOne(){
+    button1.addEventListener("click", "")
+    button2.addEventListener("click", "")
+    button3.addEventListener("click", "")
+
+
+    
+};
+
+//question 2 function
+
+
+
+//question 3 function
+
+
+//add event listeners to each button
+
+//timer function
+
+function countdown() {
+    var timeLeft = 120;
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+      else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } 
+      else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
+
+//move on to the next function
+
+questionNumberOne ();
